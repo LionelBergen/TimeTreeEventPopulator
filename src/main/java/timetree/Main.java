17e09aa6043574030faf7369ff077fe743fb13a3
dustytrash <Lionelbergen@live.com>
2023-09-17 12:18:31 -0500

    got webpage open working

--- src/main/java/timetree/Main.java
+++ src/main/java/timetree/Main.java
@@ -1,7 +1,19 @@
 package timetree;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.SessionNotCreatedException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("hello world");
+    WebDriverManager.firefoxdriver().setup();
+    WebDriver driver = new FirefoxDriver();
+
+    driver.get("https://www.google.com");
+
+    System.out.println("Ending program.");
+    driver.close();
   }
 }
