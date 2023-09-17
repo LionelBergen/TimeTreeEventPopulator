ca315dce0513f2a9756165cb8392051d6defa5b0
dustytrash <Lionelbergen@live.com>
2023-09-17 12:20:26 -0500

    spotlessApply

--- src/main/java/timetree/Main.java
+++ src/main/java/timetree/Main.java
@@ -1,9 +1,7 @@
 package timetree;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.SessionNotCreatedException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
